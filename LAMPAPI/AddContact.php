cfa4a30a17980070070dcdf9e1d31e4f91b6b09c
orionb05 <orionbrown05@gmail.com>
2026-02-13 16:26:45 -0500

    updated inital api to support new database

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -4,14 +4,15 @@ header("Content-Type: application/json; charset=UTF-8");
 $inData = json_decode(file_get_contents('php://input'), true);
 
 // Assumes the frontend requests with the user's id for the new contact
-$userId     = $inData["userId"];
-$firstName  = $inData["firstName"];
-$lastName   = $inData["lastName"];
+$UserID     = $inData["UserID"];
+$FirstName  = $inData["FirstName"];
+$LastName   = $inData["LastName"];
 
-include 'db.php';
+$conn = new mysqli("localhost", "DBuser", "passwordpassword", "CONTACTS_DB"); 
+if( $conn -> connect_error) {returnWithError( $conn -> connect_error);}
 
-$stmt = $conn->prepare("INSERT INTO Contacts (userId, firstName, lastName) VALUES (?, ?, ?)");
-$stmt->bind_param("iss", $userId, $firstName, $lastName);
+$stmt = $conn->prepare("INSERT INTO Contacts (UserID, FirstName, LastName) VALUES (?, ?, ?)");
+$stmt->bind_param("iss", $UserID, $FirstName, $LastName);
 
 if ($stmt->execute()) {
     echo json_encode(["status" => "success", "message" => "Contact added"]);
